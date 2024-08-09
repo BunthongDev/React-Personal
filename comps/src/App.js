e90b102db7407f325c740d1d580564da7d89c9ae
AnB School 0014 <anbschool0014@AnBs-Mac-mini-42.local>
2024-08-09 17:07:38 +0700

    Children Prop

---
+++ comps/src/App.js
@@ -0,0 +1,25 @@
+import Button from "./Button"
+
+function App() {
+  return (
+    <div>
+        <div>
+            <Button text="Click here"/>
+        </div>
+        <div>
+            <Button text="Button"/>
+        </div>
+        <div>
+            <Button text="Buy Now"/>
+        </div>
+        <div>
+            <Button text="Push "/>
+        </div>
+        <div>
+            <Button text="Call Me Now"/>
+        </div>
+    </div>
+  )
+}
+
+export default App
